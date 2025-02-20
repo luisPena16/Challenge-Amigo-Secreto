967fa6061b14f840398a53f58e5e2b8dc6778d3c
luisPena16 <luisp3325@gmail.com>
2025-02-19 19:25:05 -0600

    Add function recorreArray in app.js

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigosSecretos = [];
+let ElementoHTML = '';
 
 function agregarAmigo() {
-    let ElementoHTML = document.getElementById('amigo').value;
+    ElementoHTML = document.getElementById('amigo').value;
     
     //Ingresar al array
     if (ElementoHTML !== '') {
         amigosSecretos.push(ElementoHTML);
+        
     } else {
         alert('por favor, ingresa un nombre');
     }
 
     limpiarCaja();
+    recorreArray();
+   
     console.log(amigosSecretos);
     return;
 }
 
 function limpiarCaja(){
     document.getElementById('amigo').value = '';
+}
+
+function recorreArray() {
+    let ElementoListaAmigos = document.getElementById('listaAmigos');
+    ElementoListaAmigos.innerHTML = "";
+
+    for (let i = 0; i < amigosSecretos.length; i++) {
+        let nuevoAmigo = document.createElement('li');
+        nuevoAmigo.textContent = amigosSecretos[i];
+        ElementoListaAmigos.appendChild(nuevoAmigo);
+    }
 }
 
